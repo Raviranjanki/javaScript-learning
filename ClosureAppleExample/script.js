9346fdf123aecdfa01fc9a27d72e70bbd28fdcd6
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-08-30 16:25:52 +0530

    real life closure example

---
+++ ClosureAppleExample/script.js
@@ -0,0 +1,11 @@
+buyingApple = () => {
+  let apples = 50;
+  home = () => {
+    jar = (pour) => {
+      return "Juice";
+    };
+    let glass = jar(apples - fewApples);
+  };
+  home();
+};
+buyingApple();
