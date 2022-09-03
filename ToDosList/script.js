5f41a37dea4e6c8ed6dce28ac2a9a6b9178326a0
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-09-03 14:52:24 +0530

    User will be able to add child task of the parent task and reorder the parent task/ up and down

--- ToDosList/script.js
+++ ToDosList/script.js
@@ -4,15 +4,20 @@ const tasks = {
         id: 1,
         isDeleted: false,
         desc: "desc",
+        subDesc: 'subdesc',
         weight: 1,
     },
+    childTask: {
+        desc: '',
+    },
     element: document.getElementById("js-show-task"),
-    get: function (desc, weight) {
+    get: function (desc, weight, childTask) {
         this.task = {
             id: this.items.length,
             isDeleted: false,
             desc: desc,
             weight: weight,
+            subDesc: childTask,
         };
         this.add();
     },
@@ -26,49 +31,125 @@ const tasks = {
         this.render(this.task);
     },
     render: function (task) {
-        if (!Array.isArray(task)) {
+        this.element.innerHTML = "";
+        let f_index = 1;
+        let l_index = this.items.length;
+        this.items.forEach((item) => {
+            f_index = this.items.indexOf(item)
             const li = document.createElement("li");
             const p = document.createElement("p");
             const btn_delete = document.createElement("button");
             btn_delete.textContent = "Delete";
-            p.innerText = task.desc;
-            li.appendChild(p);
-            li.appendChild(btn_delete);
-            this.element.appendChild(li);
+            const div = document.createElement("div");
+            const up = document.createElement("button");
+            up.innerHTML = `<i class="fa-solid fa-angle-up"></i>`;
+            const down = document.createElement("button");
+            down.innerHTML = `<i class="fa-solid fa-angle-down"></i>`
+            p.innerText = item.desc;
+
+            const btn_sub_desc = document.createElement("button");
+            btn_sub_desc.innerText = 'Add';
+
+            btn_sub_desc.onclick = () => {
+                btn_sub_desc.style.display = 'none'
+                this.subDesc(item, p);
+            }
             btn_delete.onclick = () => {
-                this.delete(p, task);
+                this.delete(p, item);
             };
-            return;
+            up.onclick = () => {
+                this.moveUp(item.id);
+            };
+            down.onclick = () => {
+                this.moveDown(item.id);
+            };
+
+            if (item.subDesc != undefined) {
+                const sub_desc = document.createElement("p");
+                sub_desc.innerText = item.subDesc.desc;
+                p.append(sub_desc)
+            }
+            if (item.isDeleted) {
+                p.style.textDecoration = "line-through";
+            }
+
+
+            if (f_index == 0) {
+                up.style.visibility = 'hidden';
+            }
+            if (l_index - 1 == f_index) {
+                down.style.visibility = 'hidden';
+            }
+
+            div.append(btn_sub_desc, up, down, btn_delete)
+            li.append(p, div);
+            this.element.appendChild(li);
+        })
+    },
+    subDesc: function (item, p) {
+        const sub_desc = document.createElement('textarea')
+        const btn_save = document.createElement('button')
+
+        btn_save.textContent = 'Save'
+        sub_desc.style.display = 'block'
+        p.append(sub_desc, btn_save)
+
+        btn_save.onclick = () => {
+            this.childTask.desc = sub_desc.value
+            this.items[item.id].subDesc = this.childTask
+            this.render()
         }
-        this.element.innerHTML = "";
-        this.items.forEach((task1) => {
-            this.render(task1);
-        });
     },
     delete: function (item, task) {
         task.isDeleted = true;
         item.style.textDecoration = "line-through";
     },
     ascSort: function () {
+        const items = this.items;
         for (let i = 0; i < this.items.length - 1; i++) {
-            if (this.items[i].weight > this.items[i + 1].weight) {
-                let temp = this.items[i];
-                this.items[i] = this.items[i + 1];
-                this.items[i + 1] = temp;
+            if (items[i].weight > items[i + 1].weight) {
+                let temp = items[i];
+                items[i] = items[i + 1];
+                items[i + 1] = temp;
                 i = -1;
             }
         }
-        this.render(this.items);
+        this.render(items);
     },
     descSort: function () {
-        for (let i = 0; i < this.items.length - 1; i++) {
-            if (this.items[i].weight < this.items[i + 1].weight) {
-                let temp = this.items[i];
-                this.items[i] = this.items[i + 1];
-                this.items[i + 1] = temp;
+        const items = this.items;
+        for (let i = 0; i < items.length - 1; i++) {
+            if (items[i].weight < items[i + 1].weight) {
+                let temp = items[i];
+                items[i] = items[i + 1];
+                items[i + 1] = temp;
                 i = -1;
             }
         }
-        this.render(this.items);
+        this.render(items);
+    },
+
+    moveDown: function (current_id) {
+        for (let i = 0; i < this.items.length - 1; i++) {
+            if (current_id == this.items[i].id) {
+                [this.items[i], this.items[i + 1]] = [this.items[i + 1], this.items[i]]
+                break;
+            }
+        }
+        this.render([])
+    },
+    moveUp: function (current_id) {
+        for (let i = 1; i < this.items.length; i++) {
+            if (current_id == this.items[i].id) {
+                [this.items[i], this.items[i - 1]] = [this.items[i - 1], this.items[i]]
+                break;
+            }
+        }
+        this.render([])
     },
 };
+
+tasks.get("ravi", 1);
+tasks.get("ravi ranjan", 2);
+tasks.get("ravi pal", 1);
+tasks.get("ravi kamal", 3);
