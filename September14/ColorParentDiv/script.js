b95db5838edf7f14d0e225e59493be71f3a319d8
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-09-14 22:22:41 +0530

    September 14 task updated

--- September14/ColorParentDiv/script.js
+++ September14/ColorParentDiv/script.js
@@ -1,32 +1,22 @@
 (function () {
     let btn_list = document.querySelectorAll('button');
     const div = document.querySelector('div');
+    const colors = []
 
-    btn_list.forEach(item => {
-        item.addEventListener('click', color);
-    })
+    btn_list.forEach(item => item.addEventListener('click', color))
 
     function color(event) {
-        let color = ((event.target.innerText).toLowerCase()) || 'red';
+        let color = ((event.target.innerText).toLowerCase()) || 'doNothing';
         colorExist(color)
     }
-
+    
     function colorExist(color) {
         if (div.classList.contains(color)) {
             return;
         }
-        if (div.classList.contains('red')) {
-            div.classList.remove('red')
-        }
-        if (div.classList.contains('green')) {
-            div.classList.remove('green')
-        }
-        if (div.classList.contains('blue')) {
-            div.classList.remove('blue')
-        }
-        if (div.classList.contains('yellow')) {
-            div.classList.remove('yellow')
-        }
+        colors.includes(color) ? 0 : colors.push(color);
+        
+        div.classList.remove(...colors)
         div.classList.add(color)
     }
 })();
--- September14/ToggleButtonText/script.js
+++ September14/ToggleButtonText/script.js
@@ -1,8 +1,8 @@
 (function () {
     const btn = document.querySelector('button')
     const text = 'mechine is ';
-    btn.addEventListener('click', (e) => {
+    btn.addEventListener('click', () => {
         btn.dataset.mechine == 'off' ? btn.dataset.mechine = 'on' : btn.dataset.mechine = 'off';
         btn.innerText = text + `${btn.dataset.mechine}`
     })
-})
+})()
