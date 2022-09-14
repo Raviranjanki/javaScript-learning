ca86328ebcf3d18188927a09ce7301c70fe5812e
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-09-14 21:40:25 +0530

    September 14 task updated

--- September14/ColorChildElement/script.js
+++ September14/ColorChildElement/script.js
@@ -1,16 +1,16 @@
 (function () {
     const listOfLi = [...document.querySelectorAll('li')]
+    const FIRST_CHILD = 0;
 
-    listOfLi.forEach((item) => {
-        item.addEventListener('click', color)
-    })
+    listOfLi.forEach((item) => item.addEventListener('click', color))
 
     function color(e) {
-        let nextChild = e.target.children[0];
-        let isNextChildUl = nextChild.children[0].children[0]
-        if (nextChild == undefined || isNextChildUl == undefined)
+        let nextChild = e.target.children[FIRST_CHILD];
+        if (nextChild == undefined)
             return
-        nextChild.children[0].style.color = 'red'
-        isNextChildUl.style.color='black'
+        nextChild.children[FIRST_CHILD].style.color = 'red'
+
+        let isNextChildUl = nextChild.children[FIRST_CHILD].children[FIRST_CHILD]
+        isNextChildUl == undefined ? 0 : isNextChildUl.style.color = 'black';
     }
 })()
