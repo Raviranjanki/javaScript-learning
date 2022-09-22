5b5f09c6d8cbff6c5c60cfe9be2732299dc55edc
Ravi Ranjan Kumar <raviranjankai0@gmail.com>
2022-09-22 18:13:28 +0530

    moving turtle with arrow key

--- moveTurtle/script.js
+++ moveTurtle/script.js
@@ -1,41 +1,45 @@
 (function () {
     window.addEventListener('keydown', moveTurtle)
-    let x = 0;
-    let y = 0;
+    let leftRight = 0;
+    let topDown = 0;
     function moveTurtle(event) {
         if (!event.key.includes('Arrow')) {
-            return
+            return;
         }
-        let flip=false;
-        let rotate=0;
-        const height = window.innerHeight
-        const width = window.innerWidth
-        if(x > height || y > width)
+        let flip = false;
+        let rotate = 0;
+
+        leftRight > window.innerWidth - 200 ? leftRight = leftRight - 5 : 0;
+        leftRight <= 0 ? leftRight = leftRight = leftRight + 5 : 0;
+        topDown > window.innerHeight - 130 ? topDown = topDown - 5 : 0;
+        topDown <= 0 ? topDown = topDown + 5 : 0;
+
         switch (event.key) {
             case 'ArrowUp':
-                rotate=-90;
-                y=y-5;
+                rotate = -90;
+                topDown = topDown - 5;
                 break;
             case 'ArrowDown':
-                rotate=90;
-                y=y+5
+                rotate = 90;
+                topDown = topDown + 5
                 break;
             case 'ArrowRight':
-                flip=false;
-                x=x+5;
+                flip = false;
+                leftRight = leftRight + 5;
                 break;
             case 'ArrowLeft':
-                flip=true;
-                x=x-5
+                flip = true;
+                leftRight = leftRight - 5
                 break;
             default:
                 break;
         }
+
         const turtle = document.querySelector('.turtle')
         turtle.setAttribute('style', `
             --rotateX: ${flip ? '180deg' : '0'};
-            --x: ${x}px;
-            --y: ${y}px;
+            --leftRight: ${leftRight}px;
+            --topDown: ${topDown}px;
             --rotate: ${rotate}deg;
             `);
     }
